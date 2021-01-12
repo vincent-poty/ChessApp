6fe0dbb53c1b2a4fde6f7bfd204eccbc4b000c60
Potyv <poty.vinent@icloud.com>
2021-01-12 21:31:54 +0100

    premier commit

--- src/Echiquier.java
+++ src/Echiquier.java
@@ -25,16 +25,18 @@ public class Echiquier extends PApplet {
         drawChess();
     }
     public void drawChess() {
+        float caseWidth = width / nbrColonnes;
+        float caseHeight = height / nbrLignes;
         for(int x = 0; x < nbrLignes; ++x) {
             for(int y = 0; y < nbrLignes; ++y) {
                 if ((x + y) % 2 == 0) {
                     fill(227, 228, 194);
                     stroke(227, 228, 194);
-                    rect((x * (width / 8)), (0 + y * 60), (width / 8), (height / 8));
+                    rect((x * caseWidth), (0 + y * caseWidth), caseWidth, caseHeight);
                 } else {
                     fill( 111,55,55);
                     stroke( 111,55,55);
-                    rect((x * (width / 8)), (0 + y * 60), (width / 8), (height / 8));
+                    rect((x * caseHeight), (0 + y * caseHeight), caseWidth, caseHeight);
                 }
             }
 
