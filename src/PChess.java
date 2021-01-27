b5a285dbfd45aa90951189382e3485b3f0b578a7
Potyv <poty.vinent@icloud.com>
2021-01-27 10:59:13 +0100

    nouveau code simple

--- src/PChess.java
+++ src/PChess.java
@@ -1,28 +1,63 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PChess extends PApplet {
-    Echiquier echiquier;
 
-    public PChess() {
-        echiquier = new Echiquier();
+    int size = 100;
+    List pieces = buildPieces();
+
+    private List buildPieces(){
+        List pieces = new ArrayList();
+        String[] names = {"T", "C", "F", "D", "R", "F", "C", "T"};
+        for (int x = 0; x < 8; ++x){
+            pieces.add(new Piece(names[x], x, 1, true));
+        }
+        for (int i; i < 8; ++i){
+           pieces.add(new Piece("P", i, 1, true));
+        }
+        return pieces;
     }
 
 
-    int nbrColonnes = 8;
-    int nbrLignes = 8;
     public void settings() {
-        this.size(600, 600);
+        this.size(8 * size, 8 * size);
     }
 
-    public void setup() {
-        textSize(15.0F);
-        textAlign(3, 3);
-    }
 
     public void draw(){
-        drawChess();
+
+        drawCases();
         drawPieces();
     }
+
+    private void drawPieces(){
+        textAlign(CENTER, CENTER);
+        textSize(size * 0.4f);
+        noStroke();
+        for (Piece p: pieces) {
+            drawPiece(p);
+        }
+    }
+    private void drawCases(){
+        noFill();
+        for(int x = 0; x < 8; ++x) {
+            for(int y = 0; y < 8; ++y) {
+                if ((x + y) % 2 == 0) fill(255);
+                else fill(0);
+                rect((x * size), (y * size), size, size);
+            }
+        }
+    }
+    private void drawPiece(Piece piece){
+            if (Piece.isBlack)  fill(165, 42, 42);
+            else fill(244, 226, 198);
+            ellipse(size * 0.5f, size * 0.5f, size * 0.8f, size * 0.8f);
+            if (Piece.isBlack) fill(255);
+            else fill(0);
+            text(piece.name, 0, 0, size, size);
+        }
 
 
     public static void main(String[] args) {
--- src/Piece.java
+++ src/Piece.java
@@ -1,32 +1,17 @@
 public class Piece {
-    String name;
-    boolean isBlack;
-    boolean isCaptured;
+    public String name;
+    public int x;
+    public int y;
+    static boolean isBlack;
 
-    public Piece(String name, boolean isBlack, boolean isCaptured) {
+    public Piece(String name, int x, int y, boolean isBlack) {
         this.name = name;
+        this.x = x;
+        this.y = y;
         this.isBlack = isBlack;
-        this.isCaptured = isCaptured;
     }
-    Piece tourBlanche = new Piece("Tour", false, false);
-    Piece chevalBlanche = new Piece("Cheval", false, false);
-    Piece fouBlanche = new Piece("Fou", false, false);
-    Piece dameBlanche = new Piece("Dame", false, false);
-    Piece roiBlanche = new Piece("Roi", false, false);
-    Piece pionBlanche = new Piece("Pion", false, false);
-
-    Piece tourNoir = new Piece("Tour", true, false);
-    Piece chevalNoir = new Piece("Cheval", true, false);
-    Piece fouNoir = new Piece("Fou", true, false);
-    Piece dameNoir = new Piece("Dame", true, false);
-    Piece roiNoir = new Piece("Roi", true, false);
-    Piece pionNoir = new Piece("Pion", true, false);
-    public static void main(String[] args) {
 
 
-
-        for (Piece item: ) System.out.println();
-    }
 }
 
 
